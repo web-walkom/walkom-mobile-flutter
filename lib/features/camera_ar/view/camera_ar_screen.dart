ed40fde77fb68c3a229c3f651a533add8e3d3f48
b0shka <vanya.palam@ya.ru>
2023-07-27 14:03:32 +0500

    Review

--- lib/features/camera_ar/view/camera_ar_screen.dart
+++ lib/features/camera_ar/view/camera_ar_screen.dart
@@ -1,8 +1,8 @@
-// import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
-import 'package:sceneview_flutter/sceneview_flutter.dart';
+// import 'package:sceneview_flutter/sceneview_flutter.dart';
 
 @RoutePage()
 class CameraARScreen extends StatefulWidget {
@@ -15,63 +15,63 @@ class CameraARScreen extends StatefulWidget {
 }
 
 class _CameraARScreenState extends State<CameraARScreen> {
-  // late ArCoreController arCoreController;
-
-  // @override
-  // void dispose() {
-  //   arCoreController.dispose();
-  //   super.dispose();
-  // }
-
-  // void _onArCoreViewCreated(ArCoreController controller) {
-  //   arCoreController = controller;
-
-  //   _addCube(arCoreController);
-  // }
-
-  // void _addCube(ArCoreController controller) {
-  //   final material = ArCoreMaterial(
-  //     color: Color.fromARGB(120, 66, 134, 244),
-  //     metallic: 1.0,
-  //   );
-  //   final cube = ArCoreCube(
-  //     materials: [material],
-  //     size: vector.Vector3(0.5, 0.5, 0.5),
-  //   );
-  //   final node = ArCoreNode(
-  //     shape: cube,
-  //     position: vector.Vector3(-0.5, 0.5, -3.5),
-  //   );
-  //   controller.addArCoreNode(node);
-  // }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     backgroundColor: Colors.white,
-  //     body: ArCoreView(
-  //       onArCoreViewCreated: _onArCoreViewCreated,
-  //     ),
-  //   );
-  // }
-
-  SceneViewController _controller = SceneViewController();
+  late ArCoreController arCoreController;
 
   @override
-  void initState() {
-    super.initState();
-    Future.delayed(const Duration(seconds: 1)).then((value) {
-      if (!mounted) return;
+  void dispose() {
+    arCoreController.dispose();
+    super.dispose();
+  }
 
-      _controller.displayDemo();
-    });
+  _onArCoreViewCreated(ArCoreController controller) {
+    arCoreController = controller;
+
+    _addCube(arCoreController);
+  }
+
+  void _addCube(ArCoreController controller) {
+    final material = ArCoreMaterial(
+      color: Color(0xFF2AA057),
+      metallic: 1.0,
+    );
+    final cube = ArCoreCube(
+      materials: [material],
+      size: vector.Vector3(0.5, 0.5, 0.5),
+    );
+    final node = ArCoreNode(
+      shape: cube,
+      position: vector.Vector3(0, 0, -1.5),
+    );
+    controller.addArCoreNode(node);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SceneView(_controller),
+      body: ArCoreView(
+        onArCoreViewCreated: _onArCoreViewCreated,
+      ),
     );
   }
+
+  // SceneViewController _controller = SceneViewController();
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   Future.delayed(const Duration(seconds: 1)).then((value) {
+  //     if (!mounted) return;
+
+  //     _controller.displayDemo();
+  //   });
+  // }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     backgroundColor: Colors.white,
+  //     body: SceneView(_controller),
+  //   );
+  // }
 }
